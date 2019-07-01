5a327d49797adf5b97aac9821fc62ccf6c4e41c1
Michael Drew Palazola <mpalazola@hotmail.com>
2019-07-01 14:09:54 -0500

    fixed issues with click events

---
+++ src/scripts/registrationpage.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+let registerButton = document.getElementById("registration-button-submit")
+
+registerButton.addEventListener("click", () => {
+
+    let userNameInput = document.getElementById("reg-user-name").value
+    let userEmailInput = document.getElementById("reg-email").value
+    console.log(userNameInput)
+    console.log(userEmailInput)
+})
--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -1,10 +1,10 @@
-import { create } from "domain";
+
 
 let welcomePageContainer = document.getElementById("welcome-page")
 
 
 function createWelcomePage() {
-    return `
+    welcomePageContainer.innerHTML =`
     <div id="welcome-pagedynamic">
     <h1>Welcome Page</h1>
     <h2>Welcome to Nutshell, please Register or Login to continue to your Dashboard</h2>
@@ -12,48 +12,57 @@ function createWelcomePage() {
     <button id="register-btn">Register</button>
     </div>
     `
+    document.getElementById("login-btn").addEventListener("click", () => {
+        createLoginPage()
+    })
+
+    document.getElementById("register-btn").addEventListener("click", () => {
+        createRegisterPage()
+    })
+
 }
 
 
-welcomePageContainer.innerHTML = createWelcomePage()
-
-let loginButton = document.getElementById("login-btn").addEventListener("click", () => {
-    createLoginPage()
-})
-
-
-let registerButton = document.getElementById("register-btn").addEventListener("click", () => {
-    createRegisterPage()
-})
 
 function createLoginPage() {
 welcomePageContainer.innerHTML = `
-   <form id="login-form">
+
    <h1>Login Form</h1>
    <input type="text" placeholder="user name">
    <input type="text" placeholder="email">
    <button id="login-btn-submit">Login</button>
-   <button id="return-to-welcomepage">Return to Welcome Page</button>
-   </form>
+   <button id="return-to-welcomepage1">Return to Welcome Page</button>
    `
+   document.getElementById("return-to-welcomepage1").addEventListener("click",
+    createWelcomePage
+    )
 }
+
+
+
+
 
 function createRegisterPage() {
     welcomePageContainer.innerHTML = `
-    <form id="registration-form">
+
     <h1>Registration Form</h1>
-    <input type="text" placeholder="new user name">
-    <input type="text" placeholder="your email address">
+    <input id="reg-user-name" type="text" placeholder="new user name">
+    <input id="reg-email" type="text" placeholder="your email address">
     <button id="registration-button-submit">Register</button>
-    <button id="return-to-welcomepage">Return to Welcome Page</button>
-    </form>
+    <button id="return-to-welcomepage2">Return to Welcome Page</button>
     `
+    document.getElementById("return-to-welcomepage2").addEventListener("click", () => {
+        console.log("you clicked the register button")
+        createWelcomePage()
+    })
+
+    document.getElementById("registration-button-submit").addEventListener("click", () => {
+        console.log("you clicked the register button")
+    })
 }
 
-let returnToWelcomePage = document.getElementById("return-to-welcomepage").addEventListener("click", () =>  {
-    createWelcomePage()
-})
 
 
 
-export {createWelcomePage}
+
+export {createWelcomePage, createLoginPage, createRegisterPage}
